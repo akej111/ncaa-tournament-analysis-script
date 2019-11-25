59519bf5c2468415388d246b3fb0f7042d20f21c
Andy <ajiang@umich.edu>
2019-11-25 10:37:44 -0500

    adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ncaa-tournament-analysis',
+    version='python-2.7',
+    packages=[''],
+    url='',
+    license='',
+    author='Andy',
+    author_email='ajiang@umich.edu',
+    description=''
+)
